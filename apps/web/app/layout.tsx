f05d08bce535b20a79f56c53c7ea62488185b6e0
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Air.local>
2026-03-31 15:40:14 +0530

    Add GTM head script and noscript iframe fallback

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -28,6 +28,12 @@ const poppins = Poppins({
   variable: '--font-poppins',
   display: 'swap',
 });
+const GTM_CONTAINER_ID = 'GTM-5HMG8JZG';
+const GTM_INIT_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');`;
 
 export async function generateMetadata(): Promise<Metadata> {
   const settings = await getSeoSettings();
@@ -279,7 +285,21 @@ async function GlobalSchemaScripts() {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Script
+        id="gp-gtm-init"
+        strategy="beforeInteractive"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: GTM_INIT_SCRIPT }}
+      />
       <body className={poppins.variable} suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_CONTAINER_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <GlobalSchemaScripts />
         <GlobalIntegrationScripts />
         <Providers>
